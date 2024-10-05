326a6d8e5f8f69554af75620810ec5f3f192b577
yukiteru11310 <uyu0614@gmail.com>
2024-10-05 10:54:47 +0900

    header

---
+++ app-test/src/components/Header.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const name= ref<string>('yuki')
+</script>
+
+<template>
+    <div id="headWrap">
+        <h1>ToDo List</h1>
+        <p>ようこそ{{ name }}さん</p>
+    </div>
+</template>
+
+<style scoped>
+    h1, p{
+        color: rgb(255, 255, 255);
+    }
+    #headWrap {
+        display: flex;
+        justify-content: space-between;
+        height: 50px;
+        background-color: rgb(31, 155, 58);
+    }
+    #headWrap h1 {
+        margin-left: 20px;
+    }
+    #headWrap p {
+        height: 100%;
+        line-height: 50px;
+        margin-right: 20px;
+    }
+</style>
